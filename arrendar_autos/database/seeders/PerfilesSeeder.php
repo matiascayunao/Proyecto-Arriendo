ca4b9c89695f5451c6d424477af098d9b494fdcc
Matias Cayunao Ogaz <m.cayunao.o@gmail.com>
2024-06-29 19:01:05 -0400

    controllers,models,seeders y migrations

---
+++ arrendar_autos/database/seeders/PerfilesSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PerfilesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
