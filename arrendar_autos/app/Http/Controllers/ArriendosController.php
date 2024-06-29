7600086e53248594e11b3b0848803a0689767189
Matias Cayunao Ogaz <m.cayunao.o@gmail.com>
2024-06-29 19:01:05 -0400

    controllers,models,seeders y migrations

---
+++ arrendar_autos/app/Http/Controllers/ArriendosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Arriendo;
+use Illuminate\Http\Request;
+
+class ArriendosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Arriendo $arriendo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Arriendo $arriendo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Arriendo $arriendo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Arriendo $arriendo)
+    {
+        //
+    }
+}
